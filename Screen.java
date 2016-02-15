364305b6473e44556e6f412387d90059b15b5736
MadHatterTenSix <beastwood2013@gmail.com>
2016-02-15 02:36:54 -0600

    Update Screen.java

--- Screen.java
+++ Screen.java
@@ -11,6 +11,8 @@
 
 package terminal;
 
+import terminal.Cursor.CursorPosition;
+
 public class Screen
 {
     public static final int MAX_ROWS = 10;
@@ -25,42 +27,6 @@ public class Screen
     public Screen()
     {
         init();
-    }
-
-    public static void overwrite(String s)
-    {
-        int row = Cursor.getRow();
-        int col = Cursor.getCol();
-
-        _text[row][col] = s;
-
-        update(_textWithCursor);
-    }
-
-    public static void eraseToEnd()
-    {
-        int row = Cursor.getRow();
-        int col = Cursor.getCol();
-
-        while (col < MAX_COLS)
-            _text[row][col++] = BLANK;
-
-        update(_textWithCursor);
-    }
-
-    public static void clear()
-    {
-        int row;
-        int col;
-
-        for (row = 0; row < MAX_ROWS; row++)
-        {
-            for (col = 0; col < MAX_COLS; col++)
-            {
-                _text[row][col] = BLANK;
-            }
-        }
-        updateCursorPos();
     }
 
     private static void init()
@@ -82,7 +48,10 @@ public class Screen
         }
     }
 
-    public static void updateCursorPos()
+    /**
+     * Public function that attempts to update display.
+     */
+    public static void updateDisplay()
     {
         if (_text == null || _textWithCursor == null)
             init();
@@ -92,29 +61,101 @@ public class Screen
 
         int row = Cursor.getRow();
         int col = Cursor.getCol();
-
         _textWithCursor[row][col] = BLOCK;
-
-        update(_textWithCursor);
+        updateDisplay(_textWithCursor);
     }
 
-    public static void update(String[][] text)
+    /**
+     * Private function that sets the final text for the display.
+     */
+    private static void updateDisplay(String[][] displayArray)
     {
+        if (displayArray == null)
+            return;
+
         StringBuilder sb = new StringBuilder();
         int row;
         int col;
 
         sb.append("");
-
         for (row = 0; row < MAX_ROWS; row++)
         {
             for (col = 0; col < MAX_COLS; col++)
-            {
-                sb.append(text[row][col]);
-            }
+                sb.append(displayArray[row][col]);
             sb.append(NEWLINE);
         }
 
         Main.terminal.setText(sb.toString());
+    }
+
+    /**
+     * Write to terminal screen.
+     */
+    public static void write(String s)
+    {
+        if (Terminal.getWriteMode() == Terminal.WriteMode.INSERT)
+            insert(s);
+        else if (Terminal.getWriteMode() == Terminal.WriteMode.OVERWRITE)
+            overwrite(s);
+
+        Cursor.right();
+        updateDisplay();
+    }
+
+    public static void write(char c)
+    {
+        String s = "" + c;
+        write(s);
+    }
+
+    /**
+     * Overwrite text at current cursor position.
+     */
+    private static void overwrite(String s)
+    {
+        CursorPosition cp = Cursor.getPosition();
+        _text[cp.row][cp.col] = s;
+    }
+
+    /**
+     * Insert text at current cursor position.
+     */
+    private static void insert(String s)
+    {
+        int row = Cursor.getRow();
+        int col = Cursor.getCol();
+        int tmpCol;
+
+        /* Shift contents to the right of cursor position to make room.*/
+        tmpCol = MAX_COLS - 1;
+        while (tmpCol > col)
+        {
+            _text[row][tmpCol] = _text[row][tmpCol-1];
+            tmpCol--;
+        }
+
+        /* Insert. */
+        _text[row][col] = s;
+    }
+
+    public static void eraseRight()
+    {
+        int row = Cursor.getRow();
+        int col = Cursor.getCol();
+
+        while (col < MAX_COLS)
+        {
+            _text[row][col++] = BLANK;
+        }
+    }
+
+    public static void clear()
+    {
+        int row;
+        int col;
+
+        for (row = 0; row < MAX_ROWS; row++)
+            for (col = 0; col < MAX_COLS; col++)
+                _text[row][col] = BLANK;
     }
 }
